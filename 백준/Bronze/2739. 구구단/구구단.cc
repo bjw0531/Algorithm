7ef533b1dec016a84b194de5222ec40e378ea434
방지원 <31693657+bjw0531@users.noreply.github.com>
2025-05-30 17:22:05 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a,b,c;
+
+int main()
+{
+    cin >> a;
+    for(int i = 1; i <= 9; i++)
+    {
+        cout << a << " * " << i << " = " << a*i << endl;
+    }
+
+}
