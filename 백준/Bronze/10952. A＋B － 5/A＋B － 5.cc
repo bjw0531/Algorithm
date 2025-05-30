2fc1716880ad9a62eb73db331d4ca056eee4d787
방지원 <31693657+bjw0531@users.noreply.github.com>
2025-05-30 17:29:27 +0900

    [Bronze V] Title: A+B - 5, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+using LL = long long;
+
+LL a,b,c,d;
+LL result;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    while(1)
+    {
+        cin >> a >> b;
+        if(!a&&!b)  break;
+        cout << a+b << endl;
+
+    }
+}
