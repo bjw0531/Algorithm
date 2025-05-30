8bd35464b58952c5dbe732dc8bb0314322b337b4
방지원 <31693657+bjw0531@users.noreply.github.com>
2025-05-30 17:21:30 +0900

    [Bronze III] Title: 곱셈, Time: 96 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,13 @@
+a = input()
+b = input()
+
+
+three = int(a) * int(b[2])
+four = int(a) * int(b[1])
+five = int(a) * int(b[0])
+six = int(a) * int(b)
+
+print(three)
+print(four)
+print(five)
+print(six)
