d7800f7a1ed995102dc1670591ea276ef58af04c
방지원 <31693657+bjw0531@users.noreply.github.com>
2025-05-30 17:33:11 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    printf("         ,r\'\"7\nr`-_   ,\'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+}
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
