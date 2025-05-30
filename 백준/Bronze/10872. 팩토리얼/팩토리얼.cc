b3289d8ae2b304bdd2fd658b72a5897234ad5c96
방지원 <31693657+bjw0531@users.noreply.github.com>
2025-05-30 17:29:03 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n)
+{
+	if (n == 0)	return 1;
+	if (n == 1)	return 1;
+	return factorial(n - 1) * n;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	int result = factorial(n);
+
+	cout << result;
+}
